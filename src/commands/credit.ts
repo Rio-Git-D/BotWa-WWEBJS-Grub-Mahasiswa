4682c87b23c3bf6c1a439a2b501682895ce3d9d1
Rio-Git-D <rio.git.done@gmail.com>
2025-01-28 03:48:30 +0000

    mengubah credit

--- src/commands/credit.ts
+++ src/commands/credit.ts
@@ -3,8 +3,8 @@ import { Client, Message } from "whatsapp-web.js";
 export const credit = (client: Client, message: Message): void => {
   const creditMessage = `
 🤖 *Bot Info*:
-- Dibuat oleh: riokentuahmi
-- NPM : 210401002
+- Dibuat oleh: Rio Kentuahmi
+- NPM : 210401001
 - GitHub: https://github.com/Rio-Git-D/BotWa-WWEBJS-Grub-Mahasiswa 
 - Teknologi: wweb.js
     `;
